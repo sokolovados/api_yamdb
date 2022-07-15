6b0fa468c1fcc1cd528fc73ae5ec27b0bc31b537
Вадим Соколов <sokolovadosolo@gmail.com>
2022-07-15 20:30:36 +0300

    add user model and migrations

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,5 +1,23 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+
+    # User role choices.
+    ADMIN = 'admin'
+    USER = 'user'
+    MODERATOR = 'moderator'
+    USER_ROLES = (
+        (ADMIN, 'admin'),
+        (USER, 'user'),
+        (MODERATOR, 'moderator'),
+    )
+    bio = models.TextField(
+        'Биография',
+        blank=True,
+    )
+    role = models.CharField(
+        max_length=256, choices=USER_ROLES,
+        default=USER
+    )
