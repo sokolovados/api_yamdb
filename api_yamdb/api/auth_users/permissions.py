a229169e9d249b29760f5047e2eea0359bd996cb
Вадим Соколов <sokolovadosolo@gmail.com>
2022-07-22 22:46:43 +0300

    Fixed "E126 continuation line over-indented for hanging indent"

--- api_yamdb/api/auth_users/permissions.py
+++ api_yamdb/api/auth_users/permissions.py
@@ -7,7 +7,7 @@ class AdminOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.user.is_authenticated:
             return (
-                    request.user.is_superuser
-                    or request.user.role == 'admin'
+                request.user.is_superuser
+                or request.user.role == 'admin'
             )
         return False
