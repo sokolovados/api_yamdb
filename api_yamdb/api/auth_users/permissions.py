1b2eaa230c7f9153d85f457d1923cfc570905562
Вадим Соколов <sokolovadosolo@gmail.com>
2022-07-28 07:26:23 +0300

    move superuser check to the models

--- api_yamdb/api/auth_users/permissions.py
+++ api_yamdb/api/auth_users/permissions.py
@@ -7,8 +7,5 @@ class AdminOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (
             request.user.is_authenticated
-            and (
-                request.user.is_superuser
-                or request.user.is_admin
-            )
+            and request.user.is_admin
         )
--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -39,12 +39,15 @@ class User(AbstractUser):
 
     @property
     def is_admin(self):
-        return self.role == 'admin'
+        return (
+            self.role == self.ADMIN
+            or self.is_superuser
+        )
 
     @property
     def is_moderator(self):
-        return self.role == 'moderator'
+        return self.role == self.MODERATOR
 
     @property
     def is_user(self):
-        return self.role == 'user'
+        return self.role == self.USER
